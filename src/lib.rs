d44c868c9668769d3afec645c398e824b49edc6b
Christian Tamblyn <ctamblyn76@gmail.com>
2021-06-21 08:09:40 +0100

    Fix clippy warnings.

--- src/lib.rs
+++ src/lib.rs
@@ -5,28 +5,25 @@
 //! ## Example
 //!
 //! ```rust
-//! use sudoku_solver::*;
+//! # use sudoku_solver::*;
+//! let board = Board::from(&[
+//!     [0, 2, 0, 0, 0, 0, 0, 0, 0], // row 1
+//!     [0, 0, 0, 6, 0, 0, 0, 0, 3], // row 2
+//!     [0, 7, 4, 0, 8, 0, 0, 0, 0], // row 3
+//!     [0, 0, 0, 0, 0, 3, 0, 0, 2], // row 4
+//!     [0, 8, 0, 0, 4, 0, 0, 1, 0], // row 5
+//!     [6, 0, 0, 5, 0, 0, 0, 0, 0], // row 6
+//!     [0, 0, 0, 0, 1, 0, 7, 8, 0], // row 7
+//!     [5, 0, 0, 0, 0, 9, 0, 0, 0], // row 8
+//!     [0, 0, 0, 0, 0, 0, 0, 4, 0], // row 9
+//! ]);
 //!
-//! fn main() {
-//!     let board = Board::from(&[
-//!         [0, 2, 0, 0, 0, 0, 0, 0, 0], // row 1
-//!         [0, 0, 0, 6, 0, 0, 0, 0, 3], // row 2
-//!         [0, 7, 4, 0, 8, 0, 0, 0, 0], // row 3
-//!         [0, 0, 0, 0, 0, 3, 0, 0, 2], // row 4
-//!         [0, 8, 0, 0, 4, 0, 0, 1, 0], // row 5
-//!         [6, 0, 0, 5, 0, 0, 0, 0, 0], // row 6
-//!         [0, 0, 0, 0, 1, 0, 7, 8, 0], // row 7
-//!         [5, 0, 0, 0, 0, 9, 0, 0, 0], // row 8
-//!         [0, 0, 0, 0, 0, 0, 0, 4, 0], // row 9
-//!     ]);
+//! println!("Puzzle:\n{}\n", board);
 //!
-//!     println!("Puzzle:\n{}\n", board);
-//!
-//!     if let Some(solution) = solve(&board) {
-//!         println!("Solution:\n{}\n", solution);
-//!     } else {
-//!         println!("No solution found.");
-//!     }
+//! if let Some(solution) = solve(&board) {
+//!     println!("Solution:\n{}\n", solution);
+//! } else {
+//!     println!("No solution found.");
 //! }
 //! ```
 
