584d115b8cfc474ebcc484960be606e4a18729a3
Christian Tamblyn <ctamblyn76@gmail.com>
2022-07-22 18:33:28 +0100

    Add benchmark for multiple solution case.

--- benches/solver_benchmarks.rs
+++ benches/solver_benchmarks.rs
@@ -37,5 +37,29 @@ fn hard_unsolvable_puzzle(c: &mut Criterion) {
     });
 }
 
-criterion_group!(benches, hard_solvable_puzzle, hard_unsolvable_puzzle);
+fn large_solution_set(c: &mut Criterion) {
+    let board = Board::from(&[
+        [0, 2, 0, 0, 0, 0, 0, 0, 0], // row 1
+        [0, 0, 0, 0, 0, 0, 0, 0, 3], // row 2
+        [0, 7, 4, 0, 8, 0, 0, 0, 0], // row 3
+        [0, 0, 0, 0, 0, 3, 0, 0, 2], // row 4
+        [0, 8, 0, 0, 4, 0, 0, 1, 0], // row 5
+        [6, 0, 0, 5, 0, 0, 0, 0, 0], // row 6
+        [0, 0, 0, 0, 1, 0, 7, 8, 0], // row 7
+        [5, 0, 0, 0, 0, 9, 0, 0, 0], // row 8
+        [0, 0, 0, 0, 0, 0, 0, 4, 0], // row 9
+    ]);
+
+    c.bench_function("large solution set", |b| {
+        b.iter(|| (black_box(SolutionIter::from(board).count())))
+    });
+}
+
+criterion_group!(
+    benches,
+    hard_solvable_puzzle,
+    hard_unsolvable_puzzle,
+    large_solution_set
+);
+
 criterion_main!(benches);
